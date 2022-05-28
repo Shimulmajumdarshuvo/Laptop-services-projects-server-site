da2025bde1359408a33fc5a4bac31a1f3a79c45b
shimul <shimul@gmail.com>
2022-05-28 23:42:22 +0600

    post method added

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ async function run() {
         console.log('database connected');
 
         const serviceCollection = client.db('Laptop-services').collection('services');
-        const bookingCollection = client.db('Laptop-services').collection('services');
+        const ordersCollection = client.db('Laptop-services').collection('booking');
+
+        const paymentCollection = client.db('Laptop-services').collection('payment');
 
 
 
@@ -38,17 +40,63 @@ async function run() {
 
         app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
+
             const filter = { _id: ObjectId(id) }
             const result = await serviceCollection.findOne(filter)
             res.send(result);
-
-        });
-
-        app.post('/booking', async (req, res) => {
-            const booking = req.body;
-            const result = await bookingCollection.insertOne(booking);
-            res.send(result);
         })
+
+        app.post('/service', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order)
+            res.send(order);
+        })
+
+
+
+        //my order
+
+        app.get('/orders/:email', async (req, res) => {
+            const email = req.params.email;
+
+            const filter = { email: email }
+            const myOrder = await ordersCollection.find(filter).toArray()
+            res.send(myOrder);
+        })
+
+
+
+
+
+        //order code 
+
+
+        app.get("/orders", async (req, res) => {
+            const orders = await ordersCollection.find({}).toArray();
+            res.send(orders);
+        });
+        app.post('/order', async (req, res) => {
+
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order)
+            res.send(result)
+        })
+
+        // app.patch('/orderPay/:id', async (req, res) => {
+        //     const id = req.params.id
+        //     const payment = req.body
+        //     const filter = { _id: ObjectId(id) }
+        //     const updateDoc = {
+
+        //         $set: {
+        //             paid: true,
+        //             transactionId: payment.transactionId
+        //         },
+        //     };
+        //     const updateOrder = await ordersCollection.updateOne(filter, updateDoc)
+        //     const result = await paymentCollection.insertOne(payment)
+        //     res.send(updateOrder)
+        // })
 
 
 
